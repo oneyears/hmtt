a4a7bd1b12340cf0fb044ca45ddc66db8f4e9c7b
liaosheng <970771737@qq.com>
2022-02-09 12:19:53 +0800

    01-初始化项目第一次提交

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// 导入flexible
+import 'amfe-flexible'
+
+// 导入自己写的全局样式
+import './style/base.less'
+
 new Vue({
   router,
   render: h => h(App)
